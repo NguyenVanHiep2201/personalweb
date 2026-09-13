aa6d3467a59b92f2fa06adcdb867cb02d8a58730
Nguyen Van Hiep <nguyenvanhiep220188@gmail.com>
2026-09-13 22:58:19 +0700

    config: prepare Astro for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,2 +1,6 @@
 import { defineConfig } from 'astro/config';
-export default defineConfig({ site: 'https://YOUR-SITE.netlify.app' });
+
+export default defineConfig({
+  site: 'https://nguyenvanhiep2201.github.io',
+  base: '/personalweb',
+});
